d605cf92bb4ab5c28a8680d435e69c4c0760e6c2
Adam <adama_sakho@hotmail.com>
2016-03-09 19:47:48 +0100

    gestion du header

--- view/front/index.php
+++ view/front/index.php
@@ -6,7 +6,7 @@
 
 
 		<div id="main-connect">
-	<div class="col-lg-6 col-md-6 col-sm-6 col-md-offset-2 col-lg-offset-2 col-sm-offset-2">
+	<div class="col-lg-6 col-md-6 col-sm-6 col-md-offset-3 col-lg-offset-3 col-sm-offset-3" style="margin-top:6%;">
 		
 		<form action="<?php echo ADRESSE_SITE; ?>index/loginCheck" method="post" name="auth" class="form-signin">
 		<h1>Connexion</h1><br>
@@ -18,14 +18,7 @@
 		</form>
 	</div>
 </div>
-		<div id="mainBottomBar">
-    
-    
-    <p class="legal homeBottomBar">
-            
-            Each Best Western<sup>&reg;</sup> branded hotel is independently owned and operated. &copy; 2002&ndash;2016 Best Western International, Inc. All rights reserved.
-            &copy;2016 <a href="http://www.tripadvisor.com/" target="_blank">TripAdvisor</a> LLC. All rights reserved &dagger;Numbers are approximate, may fluctuate and include hotels currently in the development pipeline.
-        </p>
+		
    
 
 </div>
