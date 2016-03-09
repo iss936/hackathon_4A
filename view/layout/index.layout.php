293d654c9deda86a887abb64d8617176e2c0bbfd
PatpatP <pol.patrick1411@gmail.com>
2016-03-09 16:48:29 +0100

    cconexion

--- view/layout/index.layout.php
+++ view/layout/index.layout.php
@@ -2,9 +2,9 @@
 
 //if is connected for let charge the header of the login form
 
-//if (security::is_connected()){
+if (security::is_connected()){
     include("view/inc/head.php"); 
-//}
+}
 
     include "view/".$controller."/".$action.".php";
     include("view/inc/body.php");
